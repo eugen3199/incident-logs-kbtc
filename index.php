6130d2442d92e686db40999c5bc9fd1d1b7f4e16
Kaung Htun Thant <3199busi@gmail.com>
2022-08-22 10:14:37 +0630

    index.php added

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+  <title></title>
+</head>
+<body>
+<script type="text/javascript" src="js/materialize.min.js"></script>
+</body>
+</html>
