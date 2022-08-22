0ae8e22d25bfe1417bed4b0df784cc37e329e638
Kaung Htun Thant <3199busi@gmail.com>
2022-08-22 09:41:53 +0630

    Wiped everything concerning other branches

--- admin/test2.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$sdate = htmlspecialchars($_POST['start_date']);
-    $edate = htmlspecialchars($_POST['end_date']);
-    $category = htmlspecialchars($_POST['category']);
-    $subcat = htmlspecialchars($_POST['subcat']);
-    
-	echo "$sdate, $edate, $category, $subcat";
-?>
